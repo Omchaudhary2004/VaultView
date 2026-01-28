a2ec28a556f7e6c9c1c976381529a8b16786ee1d
Omchaudhary2004 <omghanshyam394@gmail.com>
2026-01-28 21:26:38 +0530

    wallet section

---
+++ pages/wallet_section.jsx
@@ -0,0 +1,14 @@
+import Nav_bar from "./navbars";
+import Footers from "./footer";
+function Wallet_info() {
+    return(
+        <div>
+            <Nav_bar />
+            <h1>Hello</h1>
+            <Footers />
+        </div>
+    );
+
+}
+
+export default Wallet_info;
--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from '../pages/landing'
 import "../pages/landing.css"
+import {Routes ,Route ,BrowserRouter } from 'react-router-dom'
+import Wallet_info from '../pages/wallet_section'
 function App() {
 
 
   return (
-    <>
-    <Landing />
-    </>
+    <BrowserRouter>
+    <Routes>
+    <Route  path="/" element={<Landing />} />
+    <Route  path="/Solana" element={<Wallet_info />} />
+    <Route  path="/Ethereum" element={<Wallet_info />} />
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
